a6673ae4a66b208eefbab20f576d0324d502cb35
bridget462 <bridges.dev.create@gmail.com>
2020-09-18 14:24:01 +0900

    modal function added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmark-container");
 
+// Show modal, focus on Input
+function showModal() {
+  modal.classList.add("show-modal");
+  websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("show-modal");
+});
+window.addEventListener("click", (e) => {
+  return e.target === modal ? modal.classList.remove("show-modal") : false;
+});
